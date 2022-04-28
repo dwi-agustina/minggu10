d33860779b850dd4693c992d827c65881069b80a
Dwi_Agustina(E41200583) <80611108+dwi-agustina@users.noreply.github.com>
2022-04-28 20:37:18 +0700

    first commit

---
+++ acara2/android/app/src/main/kotlin/com/example/acara2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acara2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ acara2/lib/data/api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import "package:http/http.dart" show Client, Response;
+import 'package:acara2/model/popular_movies.dart';
+
+class ApiProvider { 
+  String apiKey = '0fc5740199faa752da813c8c97f659e8';
+  String baseUrl = 'https://api.themoviedb.org/3';
+
+  Client client = Client();
+
+  Future<PopularMovies> getPopularMovies() async { 
+    
+Uri url = Uri.parse('$baseUrl/movie/popular?api_key=$apiKey');
+    Response response =
+    await client.get(url);
+
+    if (response.statusCode == 200) { 
+      return PopularMovies.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+}
